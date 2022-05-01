edc6f480913d34b3baf9027158a39c05441e4386
atjpta <caoducan351@gmail.com>
2022-05-01 10:18:37 +0700

    sua lai index.js

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     app: {
-        port: process.env.PORT || 8088,
+        port: process.env.PORT || 8888,
     },
     db: {
         uri: process.env.MONGODB_URI || "mongodb+srv://admin:abc201199@cluster0.3crqh.mongodb.net/wbchat?retryWrites=true&w=majority"
